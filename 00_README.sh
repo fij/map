17fe76eac77a693ee62d394161ed55b2f04aafb7
fij <fij@elte.hu>
2017-07-20 10:13:13 +0200

    Update 00_README.sh

--- 00_README.sh
+++ 00_README.sh
@@ -1,19 +1,19 @@
 #!/bin/sh                                                                                                                                         
 
-# A small crawler that maps the hyperlinks within a given domain. Example: jnj.com (Johnson & Johnson).
+# A small crawler that maps the hyperlinks within a given domain. Example: mit.edu.
 
-nohup nice -n 19 \                                                                                                                                
-      python2.7 \                                                                                                                                 
-      01_crawler.py \                                                                                                                             
-      "http://www.jnj.com" \                                                                                                                      
-      60 \                                                                                                                                        
-      5 \                                                                                                                                         
-      2 \                                                                                                                                         
-      "jnj.com" \                                                                                                                                 
-      02_nodes.txt \                                                                                                                              
-      03_links.txt \                                                                                                                              
-      1 \                                                                                                                                         
-      1 \                                                                                                                                         
-      >04_stdout.txt \                                                                                                                            
-      2>05_stderr.txt \                                                                                                                           
+nohup nice -n 19 \
+      python2.7 \
+      01_crawler.py \
+      "http://www.mit.edu" \
+      60 \
+      5 \
+      2 \
+      "mit.edu" \
+      02_nodes.txt \
+      03_links.txt \
+      1 \
+      1 \
+      >04_stdout.txt \
+      2>05_stderr.txt \
 &
